b8ce30e8f7a9031e68fe304d5b276a1249c03d21
VincentXis <rickard.bernal@gmail.com>
2017-03-24 16:54:00 +0100

    commit to deploy and test live

--- app/product/product-details.controller.js
+++ app/product/product-details.controller.js
@@ -1,6 +1,5 @@
 angular.module("product").controller("productDetailsController", ["$scope", "$routeParams", "$location", "$q", "productService", function ($scope, $routeParams, $location, $q, productService) {
-    $scope.product = {}
-    $scope.amount = 1;
+    $scope.product = {};
     productService.getProductById($routeParams.id).then(function (response) {
         $scope.product = response.data;
     });
